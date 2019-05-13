21efc26e2c284e5c37baca9d4a351915edf42980
Erik3003 <erik.t.3003@googlemail.com>
2019-05-13 21:48:58 +0200

    Merged changes

--- server/init/passport.js
+++ server/init/passport.js
@@ -14,20 +14,6 @@ module.exports = function(passport) {
       }).then(user => {
         if (!user) {
           return done(null, false, { message: 'Incorrect username' });
-<<<<<<< HEAD
-        }
-
-        // Match password
-        console.log(password + "" + user.password)
-        bcrypt.compare(password, user.password, (err, isMatch) => {
-          if (err) throw err;
-          if (isMatch) {
-            return done(null, user);
-          } else {
-            return done(null, false, { message: 'Incorrect password' });
-          }
-        });
-=======
         } else {
           // Match password
           bcrypt.compare(password, user.password, (err, isMatch) => {
@@ -39,7 +25,6 @@ module.exports = function(passport) {
             }
           });
         }        
->>>>>>> 49ac95765dc3b07fc54174b0fa8b1d9dd3c9bf8e
       });
     })
   );
