78d45d022780bd3dcc9494c5ae10264732f2c1bf
Erik3003 <erik.t.3003@googlemail.com>
2019-06-07 14:37:16 +0200

    fixed categories

--- server/controllers/category.controller.js
+++ server/controllers/category.controller.js
@@ -8,6 +8,7 @@ const categorySchema = Joi.object({
   creator: Joi.string().required(),
   title: Joi.string().required(),
   color: Joi.string().required(),
+  _id: Joi.string()
 })
 
 module.exports = {
@@ -19,14 +20,17 @@ module.exports = {
 }
 
 async function insert(category, user) {
-  category.creator = user._id;
+  category.creator = user._id.toString();
   category = await Joi.validate(category, categorySchema, { abortEarly: false });
   return await new Category(category).save();
 }
 
 async function remove(category, user) {
-  if (!appointmentCtrl.hasAnyAppointmentCategory(category)) {
-    if (userCtrl.getUser(user)._id == getCategory(category).creator){
+  hasAny = await appointmentCtrl.hasAnyAppointmentCategory(category);
+  if (!hasAny) {
+    category = await getCategory(category);
+    user = await userCtrl.getUser(user);
+    if (category != null && user != null && user._id.toString() == category.creator.toString()){
       return await Category.deleteOne({_id: category._id});
     }
     return { Status:401 };
@@ -35,14 +39,14 @@ async function remove(category, user) {
 }
 
 async function update(category, user) {
-  category.creator = user._id;
+  category.creator = user._id.toString();
   category = await Joi.validate(category, categorySchema, { abortEarly: false });
   oldCategory = await getCategory(category);
 
   if (category == null){
     return { Status:404 };
   }
-  if (!isCreator(category, user)){
+  if (!await isCreator(category, user)){
     return { Status:401 };
   }
 
@@ -57,6 +61,7 @@ async function getCategory(category) {
   return await Category.findById(category._id);
 }
 
-function isCreator(category, user) {
-  return user._id.toString() == getCategory(category).creator.toString();
+async function isCreator(category, user) {
+  category = await getCategory(category);
+  return user._id.toString() == category.creator.toString();
 }
